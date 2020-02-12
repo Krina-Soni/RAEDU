ad5e3e9bb1b729fee4a15902b63476a26e35e8cf
denisha-addweb <denisha.addweb@gmail.com>
2020-02-12 13:40:59 +0530

    New commit by krina

--- src/test/java/AEDU/Testcases/Basecase.java
+++ src/test/java/AEDU/Testcases/Basecase.java
@@ -26,7 +26,7 @@ public class Basecase extends ReportClass{
         //driver.get("http://toolsqa.com/automation-practice-form/");
         //driver.get("http://toolsqa.com/automation-practice-table/");
         //driver.get("http://toolsqa.com/handling-alerts-using-selenium-webdriver/");
-        System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
+        //System.setProperty("Webdriver.chrome.driver",System.getProperty("user.dir")+"chromedriver");
         driver = new ChromeDriver();
         CommonVar common=new CommonVar();
         driver.get(common.url);
